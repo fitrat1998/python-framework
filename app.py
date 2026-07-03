68ea1e85e3a3dad3e97b21b4ef8c96c6145529b3
fitrat1998 <towmatovft@gmail.com>
2026-07-03 17:57:59 +0500

    routing

--- app.py
+++ app.py
@@ -2,17 +2,38 @@ from webob import Request
 from webob import Response
 
 class LoiqdevFrameApp:
+    def __init__(self):
+        self.routes = dict()
+
     def __call__(self,environ,start_response):
        request = Request(environ)
        response = self.handle_request(request)
        return response(environ,start_response)
 
-    def handle_request(selfself,request):
-        user_agent = request.environ.get("HTTP_USER_AGENT", "User Agent not Found")
-
+    def handle_request(self,request):
         response = Response()
-        response.text = f"Hello my friend with user agent {user_agent}"
 
-        return response
+        handler = self.find_handler(request)
 
+        if handler is not None:
+            handler(request,response)
+        else:
+            self.default_response(response)
+
+    def find_handler(self,request):
+                for path, handler in self.routes.items():
+                    if path == request.path:
+                        return handler
+
+
+    def default_response(self,response):
+        response.status_code =  404
+        response.text = "Not Found"
+
+    def route(self, path):
+        def wrapper(handler):
+            self.routes[path] = handler
+            return handler
+
+        return wrapper
 
--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 from app import LoiqdevFrameApp
 
 app = LoiqdevFrameApp()
+
+@app.route("/home")
+def home(request,response):
+    response.text = "Hello from home page"
+
+@app.route("/about")
+def about(request,response):
+    response.text = "Hello from about page"
